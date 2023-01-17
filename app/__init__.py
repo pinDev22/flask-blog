18e31a9cdb42f84e2e8a02bbec9ec47aaa6d8aef
Mychal Culpepper <mychalculpepper@Mychals-MBP.lan>
2023-01-17 08:25:04 -0600

    flask blog

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+# Import the Flask Class from the flask module - will be main object
+from flask import Flask
+# Import SQLAlchemy and Migrate from their modules
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+# Import the Config class from the config module - will have all of the app's configurations
+from config import Config
+
+# Create an instance of the Flask Class called app
+app = Flask(__name__)
+# Configure the app using the Config class
+app.config.from_object(Config)
+
+# Create an instance of SQLAlchemy to represent our database
+db = SQLAlchemy(app)
+# Create an instance of Migrate to represent our migration engine
+migrate = Migrate(app, db)
+# Create an instance of LoginManager to set up login functionality
+login = LoginManager(app)
+# Set the login view to redirect unauthorized users
+login.login_view = 'login'
+login.login_message = 'You must be logged in to perform this action'
+login.login_message_category = 'danger'
+
+
+# import all of the routes from the routes file into the current folder
+from . import routes, models
