360ccac897260682609b633cc487faa72b0e8ac7
Mychal Culpepper <mychalculpepper@Mychals-MBP.lan>
2023-01-26 08:15:51 -0600

    added createUser & getUser routes

--- app/routes.py
+++ app/routes.py
@@ -37,6 +37,62 @@ def signup():
 
     return render_template('signup.html', form=form)
 
+@app.route('/users/{id}', methods=["GET"])
+def getUser():
+    # Create an instance of the SignUpForm
+    form = SignUpForm()
+    # Check if a POST request AND data is valid
+    if form.validate_on_submit():
+        print('Form Submitted and Validated!')
+        # Get data from the form
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+        print(email, username, password)
+        # Query our user table to see if there are any users with either username or email from form
+        check_user = User.query.filter( (User.username == username) | (User.email == email) ).all()
+        # If the query comes back with any results
+        if check_user:
+            # Flash message saying that a user with email/username already exists
+            flash('A user with that email and/or username already exists.', 'danger')
+            return redirect(url_for('signup'))
+        # If check_user is empty, create a new record in the user table
+        new_user = User(email=email, username=username, password=password)
+        # Flash a success message
+        flash(f'Thank you {new_user.username} for signing up!', 'success')
+        # Redirect back to Home
+        return redirect(url_for('index'))
+
+    return render_template('signup.html', form=form)
+
+@app.route('/users', methods=["POST"])
+def createUser():
+    # Create an instance of the SignUpForm
+    form = SignUpForm()
+    # Check if a POST request AND data is valid
+    if form.validate_on_submit():
+        print('Form Submitted and Validated!')
+        # Get data from the form
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+        print(email, username, password)
+        # Query our user table to see if there are any users with either username or email from form
+        check_user = User.query.filter( (User.username == username) | (User.email == email) ).all()
+        # If the query comes back with any results
+        if check_user:
+            # Flash message saying that a user with email/username already exists
+            flash('A user with that email and/or username already exists.', 'danger')
+            return redirect(url_for('signup'))
+        # If check_user is empty, create a new record in the user table
+        new_user = User(email=email, username=username, password=password)
+        # Flash a success message
+        flash(f'Thank you {new_user.username} for signing up!', 'success')
+        # Redirect back to Home
+        return redirect(url_for('index'))
+
+    return render_template('signup.html', form=form)
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
